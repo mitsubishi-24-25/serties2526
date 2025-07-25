b10dd068d03a176d26cda26c7c4af041eae527b7
Clarence Jefferson T. Bulatao <clarencejefferson.bulatao.24@gmail.com>
2025-07-26 07:04:33 +0800

    Update Load names.js

--- Load names.js
+++ Load names.js
@@ -12,3 +12,18 @@ fetch('Back-To-School.json')
       container.appendChild(row);
     });
   });
+
+  fetch('July Clean-Up.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('July_Clean-Up');
+    data.forEach(student => {
+      const row = document.createElement('div');
+      row.className = 'row gx-2';
+      row.innerHTML = `
+        <div class="col-8">${student.name}</div>
+        <div class="col-4">${student.section}</div>
+      `;
+      container.appendChild(row);
+    });
+  });
