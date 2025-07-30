e0a2803b8cd2e90ab5e64f0c39cf593aa1b94d92
Clarence Jefferson T. Bulatao <clarencejefferson.bulatao.24@gmail.com>
2025-07-31 00:22:48 +0800

    Update Load names.js

--- Load names.js
+++ Load names.js
@@ -13,7 +13,7 @@ fetch('Back-To-School.json')
     });
   });
 
-  fetch('July Clean-Up.json')
+fetch('July Clean-Up.json')
   .then(response => response.json())
   .then(data => {
     const container = document.getElementById('July_Clean-Up');
@@ -28,10 +28,10 @@ fetch('Back-To-School.json')
     });
   });
 
-  fetch('Symposium_AP.json')
+fetch('Project_ALAGA.json')
   .then(response => response.json())
   .then(data => {
-    const container = document.getElementById('Symposium_AP');
+    const container = document.getElementById('Project_ALAGA');
     data.forEach(student => {
       const row = document.createElement('div');
       row.className = 'row gx-2';
@@ -40,5 +40,44 @@ fetch('Back-To-School.json')
         <div class="col-4">${student.section}</div>
       `;
       container.appendChild(row);
+    });
+  });
+
+fetch('Symposium_AP.json')
+  .then(response => response.json())
+  .then(data => {
+
+    const container1 = document.getElementById('Symposium_AP');
+    const container2 = document.getElementById('Member1');
+    const container3 = document.getElementById('Member2');
+
+    data.Symposium_AP.forEach(student => {
+      const row = document.createElement('div');
+      row.className = 'row gx-2';
+      row.innerHTML = `
+        <div class="col-8">${student.name}</div>
+        <div class="col-4">${student.section}</div>
+      `;
+      container1.appendChild(row);
+    });
+
+    data.ver1.forEach(student => {
+      const row = document.createElement('div');
+      row.className = 'row gx-2';
+      row.innerHTML = `
+        <div class="col-8">${student.name}</div>
+        <div class="col-4">${student.section}</div>
+      `;
+      container2.appendChild(row);
+    });
+
+    data.ver2.forEach(student => {
+      const row = document.createElement('div');
+      row.className = 'row gx-2';
+      row.innerHTML = `
+        <div class="col-8">${student.name}</div>
+        <div class="col-4">${student.section}</div>
+      `;
+      container3.appendChild(row);
     });
   });
