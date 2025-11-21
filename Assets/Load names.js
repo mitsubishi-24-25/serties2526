34571767d03e43189aa2af22bac1d3f72e6aa6dd
MITSUBISHI <clarencejefferson.bulatao.24@gmail.com>
2025-11-21 23:45:50 +0800

    Add files via upload

--- Assets/Load names.js
+++ Assets/Load names.js
@@ -13,6 +13,21 @@ fetch('Certificates/json/3rd_Q_Safecclass.json')
     });
   });
 
+fetch('Certificates/json/Search_3rd_Q_Safecclass.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('Sections-Search');
+    data.forEach(student => {
+      const row = document.createElement('div');
+      row.className = 'row gx-2';
+      row.innerHTML = `
+        <div class="col-8">${student.name}</div>
+        <div class="col-4">${student.section}</div>
+      `;
+      container.appendChild(row);
+    });
+  });
+
 fetch('Certificates/json/Info_Distribution.json')
   .then(response => response.json())
   .then(data => {
